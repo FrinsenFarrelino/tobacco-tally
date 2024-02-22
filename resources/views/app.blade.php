a1cec0a5a4c178256780571109aab095086c77f1
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2024-02-22 11:54:22 +0700

    coba

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,8 @@
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tobacco Tally</title>
-    @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
-    @inertiaHead
 </head>
 <body>
-    @inertia
+    test
 </body>
 </html>
