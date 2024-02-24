dcfc4ed38fbecb6a646deca7de2f31d7990c4079
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2024-02-24 09:54:44 +0700

    test coba

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.jsx')
     @inertiaHead
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
