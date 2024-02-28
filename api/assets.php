6cb3f3d869d4c6e5c7b8347e2db9dc6b9f0d4113
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2024-02-29 00:18:13 +0700

    test lagi

--- api/assets.php
+++ api/assets.php
@@ -1,7 +1,8 @@
 <?php
-// File: api/assets.php
-$queries = array();
-parse_str($_SERVER['QUERY_STRING'], $queries);
-$file = $queries['file'];
-
-echo require __DIR__ . '/../' . $file;
+if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'js') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+}
