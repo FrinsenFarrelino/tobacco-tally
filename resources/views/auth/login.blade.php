89f4a42e2edd9a9294f1e35a9b7b11c04e25f0f8
FrinsenFarrelino <frinsenfarrelino@gmail.com>
2024-04-13 13:56:52 +0700

    login brand

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,8 @@
         <div class="row">
           <di class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              TY
+              <!-- <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle"> -->
             </div>
             @if(session()->has('success'))
             <div class="alert alert-success alert-dismissible show fade" id="alert">
